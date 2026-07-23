5f4d366846ab4e46f7dbbf3bd763c1003ed9e331
Xin <xs7tng@virginia.edu>
2026-07-23 02:12:19 -0400

    M4: Add comments to batch_attn_forward

--- nano_vllm/paged/paged_attention.py
+++ nano_vllm/paged/paged_attention.py
@@ -17,28 +17,28 @@ ENGINE_CTX = AttnContext()  # Set ENGINE_CTX before each GPU forward step.
 
 def batch_attn_forward(
     self,
-    hidden_states: torch.Tensor,         # shape: [batch, seq, hidden_size]  
+    hidden_states: torch.Tensor,         # shape: [batch=1, total_tokens, hidden_size]  
     position_embeddings: tuple[torch.Tensor, torch.Tensor],
     **kwargs 
-):
+): # -> [batch=1, total_tokens, hidden_size] 
     """
     Use batch_attn_forward since M4. 
     """
-    input_shape = hidden_states.shape[:-1]              # [batch, seq]
-    hidden_shape = (*input_shape, -1, self.head_dim)    # [batch, seq, -1, head_dim]
+    input_shape = hidden_states.shape[:-1]              # [batch=1, total_tokens]
+    hidden_shape = (*input_shape, -1, self.head_dim)    # [batch=1, total_tokens, -1, head_dim]
 
     # 1. Calculate q,k,v 
-    query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1,2) # [batch, num_heads, seq, head_dim]
-    key_states   = self.k_proj(hidden_states).view(hidden_shape).transpose(1,2) # [batch, num_kv_heads, seq, head_dim]
-    value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1,2) # [batch, num_kv_heads, seq, head_dim]
+    query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1,2) # [batch=1, num_heads, total_tokens, head_dim]
+    key_states   = self.k_proj(hidden_states).view(hidden_shape).transpose(1,2) # [batch=1, num_kv_heads, total_tokens, head_dim]
+    value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1,2) # [batch=1, num_kv_heads, total_tokens, head_dim]
 
     # Apply RoPE
     cos, sin = position_embeddings
-    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin) # [batch, num_heads, seq, head_dim]
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin) # [batch, num_heads, total_tokens, head_dim]
 
-    q = query_states.transpose(1,2)[0]     # [seq, num_heads, head_dim]
-    k = key_states.transpose(1,2)[0]       # [seq, num_kv_heads, head_dim]
-    v = value_states.transpose(1,2)[0]     # [seq, num_kv_heads, head_dim]
+    q = query_states.transpose(1,2)[0]     # [total_tokens, num_heads, head_dim]
+    k = key_states.transpose(1,2)[0]       # [total_tokens, num_kv_heads, head_dim]
+    v = value_states.transpose(1,2)[0]     # [total_tokens, num_kv_heads, head_dim]
     
     # 2. Write new token's KV into KVCache.
     ctx = ENGINE_CTX    
@@ -59,11 +59,10 @@ def batch_attn_forward(
         max_seqlen_k=ctx.max_seqlen_k,
         block_table=ctx.block_table,
         causal = True,
-    ) # -> [batch, seq, num_heads, head_dim]
+    ) # -> [total_tokens, num_heads, head_dim]
 
-    # Convert output's shape from [batch, seq, num_heads, head_dim] to [batch, seq, hidden_size]
-    attn_output = out.reshape(*input_shape, -1).contiguous() #TODO: when to use contiguous? 
-    attn_output = self.o_proj(attn_output)
+    # Convert output's shape from [total_tokens, num_heads, head_dim] to [batch=1, total_tokens, hidden_size] hidden_size = num_heads * head_dim 
+    attn_output = out.reshape(*input_shape, -1).contiguous() 
     return attn_output, None 
     
 
